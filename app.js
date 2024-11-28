b17b342a189dbdacedeabf5ef72ee2a86c8397f4
Ishan Saini <ishansaini0105@gmail.com>
2024-11-28 17:33:38 +0530

    The socket for frontend was s etup and player validation jut like giving white or black side was done along move validation and comments for every step is briefly given

--- app.js
+++ app.js
@@ -19,10 +19,80 @@ let currentplayer= 'W';
 
 app.use(express.static(path.join(__dirname,'public')));
 
+
+// main route 
 app.get('/' , (req, res)=>{
     res.render('index.ejs', {title:"Chess Buster"})
 })
 
 
+// this si the code of backedn socket setup
+io.on("connection",(uniquesocket)=>{
+    console.log("connected")
+
+    // connect feature
+
+    // uniquesocket.on('Hare Krishna', ()=>{
+    //     io.emit('Hare Ram')
+    // })  this is reiceving the data from client side and giving respobnse based on it..
+
+    // disconnect feature
+
+    // uniquesocket.on('disconnect', ()=>{
+    //     console.log('Disconnected')
+    // })
+
+    // this is the code for handling the chess game logic on server side
+    if(!players.white){
+        // if white field is not assigned to player then assign it with the unique socketid of player
+        players.white= uniquesocket.id; // its unique because we do not want to assign same color to both
+        uniquesocket.emit("playerrole","W") //emit the given ifo to all users
+    }
+
+    else if(!players.black){
+        players.black=uniquesocket.id;
+        uniquesocket.emit("playerrole","B") // same for black
+    }
+    else{
+        uniquesocket.emit("Spectatorrole") // same for spectator
+    }
+
+// the logic of game if someone has left the game in betweeen
+    uniquesocket.on('disconnect', ()=>{
+// if wehite left
+        if(uniquesocket.id===players.white){
+            delete players.white;
+        }
+ // if black left       
+        else if(uniquesocket.id===players.black){
+            delete players.black;
+        }
+    // spectator does not affect the game
+    })
+
+
+  // logic for valid or invalid moves
+  
+  uniquesocket.on('move', (move)=>{
+// try catch is very importetnt otherwise the server will crash and we do not want that to happen
+    try {
+// chess.js has provided these functionalities        
+if(chess.turn('W') && uniquesocket.id!== players.white) return;
+if(chess.turn('B') && uniquesocket.id!== players.black) return;
+// it means if it is black turn and white is moving then its piece would came back to original place
+    
+const result= chess.move(move); // it holds the result valid or not, we will handle it further
+
+
+} catch (error) {
+        
+    }
+  })
+})
+
+// same code is installedin frontend as well as form of cdn 
+// when the browsert loads index.js it has a script attcahed to it chessgame.js which has the
+// front end or client initialization of socket.io and it will automatically connect two player
+// who will load same front end page along with backedn as well to handle real time operations
 
 server.listen(3000, ()=> console.log("Working fine"))
---
+++ public/javascript/chessgame.js
@@ -0,0 +1,9 @@
+const socket=io()
+// this single line willl connect various front e4nds to backed scket.io setup
+
+// socket.emit("Hare Krishna")
+
+// socket.on("Hare Ram", ()=>{
+//     console.log("Hare Rama Hare Krishna")
+// })  this wa sthe example of sending dat to server asnd then server sending it again to all the user
+// present in the group or whatever....e.g. telegram or whatsapp etc..
